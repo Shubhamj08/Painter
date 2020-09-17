8029b96d2bdbfad4c12743cd01dc2d80c9377965
Shubhamj08 <sj951563@gmail.com>
2020-09-17 15:12:19 +0530

    painter

---
+++ README.md
@@ -0,0 +1,4 @@
+# Painter
+
+## Just draw things and rotate phone to clear screen
+## Created using Canvas Object and Custom View class
--- app/src/main/java/com/android/painter/MainActivity.kt
+++ app/src/main/java/com/android/painter/MainActivity.kt
@@ -2,10 +2,16 @@ package com.android.painter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myPaintView = MyPaintView(this)
+        myPaintView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myPaintView.contentDescription = getString(R.string.paintViewContentDescription)
+
+        setContentView(myPaintView)
     }
 }
